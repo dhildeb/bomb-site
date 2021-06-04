2ad85c48e936726d0c99bcb952e82039fbf28ef2
daniel hildebrandt <d.hildebrandt93@gmail.com>
2021-06-04 13:36:42 -0600

    server test 1

---
+++ server/controllers/CommentsController.js
@@ -0,0 +1,52 @@
+import BaseController from '../utils/BaseController'
+import { Auth0Provider } from '@bcwdev/auth0provider'
+import { commentsService } from '../services/CommentsService'
+
+export class CommentsController extends BaseController {
+  constructor() {
+    super('/api/comments')
+    this.router
+      .getAll('', this.getAll)
+      .use(Auth0Provider.getAuthorizedUserInfo)
+      .post('', this.create)
+      .put('/:commentId', this.vote)
+      .delete('/:commentId', this.delete)
+  }
+
+  async getAll(req, res, next) {
+    try {
+      const comments = await commentsService.getAll()
+      return res.send(comments)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async create(req, res, next) {
+    try {
+      const comment = await commentsService.create(req.body)
+      return res.send(comment)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async vote(req, res, next) {
+    try {
+      const vote = await commentsService.vote(req.params.commentId, req.body)
+      return res.send(vote)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async delete(req, res, next) {
+    try {
+      req.body.creatorId = req.userinfo.id
+      const comment = await commentsService.delete(req.params.commentId, req.userinfo.id)
+      return res.send(comment)
+    } catch (error) {
+      next(error)
+    }
+  }
+}
---
+++ server/services/CommentsService.js
@@ -0,0 +1,23 @@
+import { dbContext } from '../db/DbContext'
+
+class CommentsService {
+  async getAll(query = {}) {
+    return await dbContext.Comments.find(query)
+  }
+
+  async create(commentData) {
+    return await dbContext.Comments.create(commentData)
+  }
+
+  async vote(commentId, voteData) {
+    const comment = await dbContext.comments.findById(commentId)
+    voteData.toString() === 'like' ? comment.likes++ : comment.dislikes++
+    return comment
+  }
+
+  async delete(commentId, userId) {
+    return await dbContext.Comments.findOneAndDelete({ _id: commentId, creatorId: userId })
+  }
+}
+
+export const commentsController = new CommentsService()
