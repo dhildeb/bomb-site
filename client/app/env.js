6642cbe7483d083bc2cad4ac68762864aeec52d8
Daniel Hildebrandt <81178293+dhildeb@users.noreply.github.com>
2021-06-04 09:53:04 -0600

    Update env.js

--- client/app/env.js
+++ client/app/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
-export const domain = ''
-export const audience = ''
-export const clientId = ''
+export const domain = 'dhild.us.auth0.com'
+export const audience = 'https://codeworks-dummy.com'
+export const clientId = '58l4DWwD0mqroCiyV32WXJBHi1fdWMjD'
