86bb2e7a78e1738e352487dc3d232927ede0f654
daniel hildebrandt <d.hildebrandt93@gmail.com>
2021-06-04 19:53:31 -0600

    comments

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -5,8 +5,8 @@ export const CommentSchema = new Schema(
   {
     comment: { type: String, required: true },
     bombId: { type: Schema.Types.ObjectId, ref: 'Bomb', required: true },
-    likes: { type: Number },
-    dislikes: { type: Number },
+    likes: { type: Number, default: 0 },
+    dislikes: { type: Number, default: 0 },
     creatorId: { type: Schema.Types.ObjectId, ref: 'Account', required: true }
   }
 )
