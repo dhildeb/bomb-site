6e86a6bb710229f73ca7bfd89f06c9aeccaa8f1e
daniel hildebrandt <d.hildebrandt93@gmail.com>
2021-06-04 22:15:20 -0600

    console

--- client/app/Controllers/CommentsController.js
+++ client/app/Controllers/CommentsController.js
@@ -1,11 +1,9 @@
-import { logger } from "../Utils/Logger";
-
 export class CommentsController {
   async vote(bombId, voteData) {
     try {
       await commentsService.vote(bombId, voteData)
     } catch (error) {
-      logger.log(error)
+      console.log(error)
     }
   }
 }
